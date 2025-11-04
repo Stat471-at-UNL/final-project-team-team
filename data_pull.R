4b9e89de21e527975f64c9f502e2222920f22300
Grey Gergen <ggergen22@gmail.com>
2025-11-04 16:28:22 -0600

    data pull updates

--- data_pull.R
+++ data_pull.R
@@ -1,10 +1,15 @@
-install.packages("rcompendium")
+# Install package to add file to .gitignore if not already installed
+#install.packages("rcompendium")
 library(rcompendium)
 
+# URL to download NYC AirBNB listings
 url <- "https://data.insideairbnb.com/united-states/ny/new-york-city/2025-10-01/visualisations/listings.csv"
-
 destination<- "NYClistings.csv"
-
 download.file(url, destination)
 
+# add data to .gitignore
 add_to_gitignore("NYClistings.csv")
+
+# Data read
+
+df <- read.csv("NYClistings.csv")
